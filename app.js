77fd73850d8915852e3c5214be7888ec139c7f61
bradymat <bradymat@Bradys-MacBook-Air.local>
2016-09-28 03:57:20 +1300

    progress but too late sleeeeeep

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var request = require('request'),
 cheerio = require('cheerio'),
 urls = []
-cardInfo = {
-  names: [],
-  imgs: []
+// cardInfo = []
+
+// All booster pack card list urls
+for (var i = 0; i < 11; i++) {
+  let count = i
+  request('http://yugioh.wikia.com/wiki/TCG_Set_Galleries:_Boosters', function(err, resp, body){
+    if(!err && resp.statusCode == 200){
+      var $ = cheerio.load(body)
+      $('a:nth-child(3)', `#gallery-${count}`).each(function(){
+        var set = (`http://yugioh.wikia.com${this.attribs.href}`)
+        urls.push(set)
+        console.log(urls)
+        console.log(urls.length)
+        })
+      }
+    })
 }
 
 // All starter and structure deck card list urls
@@ -15,54 +28,31 @@ for (var i = 0; i < 2; i++) {
       $('a:nth-child(3)', `#gallery-${count}`).each(function(){
         var url = (`http://yugioh.wikia.com${this.attribs.href}`)
         urls.push(url)
-      })
-      // console.log(urls)
-      // console.log(urls.length)
-      getCardInfo(count, body)
-
-    }
-  })
-}
-
-// All booster pack card list urls
-for (var i = 0; i < 11; i++) {
-  let count = i
-  request('http://yugioh.wikia.com/wiki/TCG_Set_Galleries:_Boosters', function(err, resp, body){
-    if(!err && resp.statusCode == 200){
-      var $ = cheerio.load(body)
-      $('a:nth-child(3)', `#gallery-${count}`).each(function(){
-        var url = (`http://yugioh.wikia.com${this.attribs.href}`)
-        urls.push(url)
-      })
-      // console.log(urls)
-      // console.log(urls.length)
-
-
-      // Getting Starter deck Yugi Card Names
-      request(`${urls[count]}`, function(err, resp, body){
-        if(!err && resp.statusCode == 200){
-          // console.log($('a > img', `#gallery-${count}`))
-          getCardInfo(count, body)
-        }
-      })
-
-
-
-
-    }
-  })
+        console.log(urls)
+        console.log(urls.length)
+        })
+      }
+    })
 }
 
 
-
-
-
-getCardInfo = function (count, body){
-  var $ = cheerio.load(body)
-  $('a > img', `#gallery-${count}`).each(function(){
-    console.log(this.attribs["data-src"])
-  $('a:nth-child(4)', `#gallery-${count}`).each(function(){
-    console.log("cards!!!", this.attribs.title)
-  })
-})
-}
+// Grabs Image and Name from wrong place
+// getCardInfo = function (count, urls){
+//   request(`${urls[count]}`, function(err, resp, body){
+//     if(!err && resp.statusCode == 200){
+//       var $ = cheerio.load(body)
+//       // Pull Image
+//       $('a > img', `#gallery-${count}`).each(function(){
+//         var image = (this.attribs["data-src"])
+//       // Pull Card Name
+//       $('a:nth-child(4)', `#gallery-${count}`).each(function(){
+//         var name = (this.attribs.title)
+//         // console.log('CCCCCCard', card)
+//         // console.log('PPPPPPPic', image)
+//         cardInfo.push({'name': name, 'image': image})
+//         console.log(cardInfo)
+//         })
+//       })
+//     }
+//   })
+// }
